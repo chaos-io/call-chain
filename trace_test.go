0aa5d1a2cdc14e9fa56895ac326c9b375a2b168b
Eric <>
2023-01-01 14:25:48 +0800

    update trace

---
+++ trace_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func A1() {
+	defer Trace()()
+	B1()
+}
+
+func B1() {
+	defer Trace()()
+	C1()
+}
+
+func C1() {
+	defer Trace()()
+	D()
+}
+
+func D() {
+	defer Trace()()
+}
+
+func TestTrace(t *testing.T) {
+	A1()
+}
